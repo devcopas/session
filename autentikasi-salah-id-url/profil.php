0181fb94f2d0212ab4cfbdf9f020ae2f3e4c2bec
devcopas <borjutimothy@gmail.com>
2021-04-12 23:19:53 +0700

    autentikasi salah

--- autentikasi-salah-id-url/profil.php
+++ autentikasi-salah-id-url/profil.php
@@ -1,3 +1,17 @@
+<?php
+include 'connection.php';
+
+$user_id 		= $_GET['user_id'];
+
+$pdo				= $db->prepare('SELECT * FROM user WHERE id=:user_id');
+$data['user_id']	= $user_id;
+					  $pdo->execute($data);
+
+$user				= $pdo->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+
 <html>
     <head>
         <title>Autentikasi - List</title>
@@ -14,7 +28,9 @@
             
             <div class="content">
 				<?php if(isset($_GET['user_id'])) : ?>
-					<p>Ini adalah Halaman Profil</p>
+					<p>User ID: <?= $user['id']; ?></p>
+					<p>Name: <?= $user['name']; ?></p>
+					
 				<?php else: ?>
                 	<p class="danger">Anda tidak dapat mengakses halaman ini</p>
                 	<a href="login.php" class="btn-login">Login</a>
